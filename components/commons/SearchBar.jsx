130d273b95da4a3b4fae91de0b95fb2740ec46f9
--alish <--alishacharya12171217@gmail.com>
2026-02-09 21:49:50 +0545

    bug fix for rounded search bar

--- components/commons/SearchBar.jsx
+++ components/commons/SearchBar.jsx
@@ -30,7 +30,7 @@ export const SearchBar = ({ placeholder, onSearch, show, rounded}) => {
       />
       <button
         type="submit"
-        className="absolute right-1 top-1/2 -translate-y-1/2 mr-2 px-2 mr-4 py-1.5 bg-custom text-white rounded-md hover:bg-primary-700 transition-colors font-medium text-sm md:text-base"
+        className="absolute right-1 top-1/2 -translate-y-1/2 px-2 mr-4 py-1.5 bg-custom text-white rounded-md hover:bg-primary-700 transition-colors font-medium text-sm md:text-base"
       >
         {show ? "Search" : <Search className="w-5 h-5" />}
       </button>
